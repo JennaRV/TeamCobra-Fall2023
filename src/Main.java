cb59ae6c4155e4edcc21930b10898ff050290d67
Syeda <skazmi@ggc.edu>
2023-10-08 10:06:58 -0400

    Khadija added the main method and added a comment in the cloned code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void main(String[] args) {
 
+    }
 //Jenna made a change to the code
 //Krishan made changes to the cloned code.
+//Khadija added the main method in order to make changes to the cloned code.
 }
